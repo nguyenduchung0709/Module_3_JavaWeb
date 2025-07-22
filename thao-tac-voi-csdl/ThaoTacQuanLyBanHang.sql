379c8c776bf2672c02ca80fd16ce09cca037ba7d
nguyenduchung0709 <15.10a13.hungducnguyen@gmail.com>
2025-07-22 12:06:02 +0700

    thao tac quan ly ban hang

--- thao-tac-voi-csdl/ThaoTacQuanLyBanHang.sql
+++ thao-tac-voi-csdl/ThaoTacQuanLyBanHang.sql
@@ -56,5 +56,41 @@ values (1, 1, 3),
 		(2, 5, 4),
 		(2, 3, 3);
 
+-- Hien thi cac thong tin cua cac hoa don trong bang Orders
+SELECT 
+    *
+FROM
+    orders;
 
+-- Hien thi danh sach khach hang da mua hang va cac san pham duoc mua
+SELECT 
+    c.cid, o.oid, c.cname, p.pid, p.pname
+FROM
+    customer c
+        JOIN
+    orders o ON c.cid = o.cid
+        JOIN
+    orderdetails od ON o.oid = od.oid
+        JOIN
+    products p ON p.pid = od.pid;
+
+-- Hien thi khach hang chua mua hang 
+SELECT 
+    c.cid, c.cname
+FROM
+    customer c
+        LEFT JOIN
+    orders o ON c.cid = o.cid
+WHERE
+    o.oid IS NULL;
+    
+-- Hien thi ma hoa don, ngay ban va gia tien
+SELECT 
+    o.oid as orderID, o.odate as orderDate, sum(p.pprice * od.odqty) as total_price
+FROM
+    orders o
+        JOIN
+    orderdetails od ON o.oid = od.oid
+    join products p on od.pid = p.pid 
+    group by o.oid, o.odate;
     
