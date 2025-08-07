29c30cc9bd7ef2559a70f37e69ef9e3815ce260a
nguyenduchung0709 <15.10a13.hungducnguyen@gmail.com>
2025-08-08 00:12:30 +0700

    tao gallery voi bootstrap

--- jdbc/src/main/java/org/example/jdbc/utils/DBC.java
+++ jdbc/src/main/java/org/example/jdbc/utils/DBC.java
@@ -1,4 +1,14 @@
 package org.example.jdbc.utils;
 
+import java.sql.Connection;
+
 public class DBC {
+    public static  Connection getConnection() {
+        Connection con = null;
+        try {
+
+        } catch (Exception e) {
+
+        }
+    }
 }
